b27675dbe2ac82f306dc3f702432fc233d9c52dd
zhale-el <zhalehelmi@gmail.com>
2025-09-14 19:08:32 +0330

    card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 const HomePage = () => {
   return (
@@ -8,7 +9,9 @@ const HomePage = () => {
       <div className="bg-primary-foreground p-4 rounded-lg  lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppBarChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Latest Transactions" />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg ">
         <AppPieChart />
       </div>
@@ -16,7 +19,9 @@ const HomePage = () => {
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2 ">
         <AppAreaChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Popular Content" />
+      </div>
     </div>
   );
 };
---
+++ components/CardList.tsx
@@ -0,0 +1,125 @@
+import Image from "next/image";
+import { Card, CardContent, CardFooter, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+
+const popularContent = [
+  {
+    id: 1,
+    title: "JavaScript Tutorial",
+    badge: "Coding",
+    image:
+      "https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 4300,
+  },
+  {
+    id: 2,
+    title: "Tech Trends 2025",
+    badge: "Tech",
+    image:
+      "https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 3200,
+  },
+  {
+    id: 3,
+    title: "The Future of AI",
+    badge: "AI",
+    image:
+      "https://images.pexels.com/photos/2007647/pexels-photo-2007647.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 2400,
+  },
+  {
+    id: 4,
+    title: "React Hooks Explained",
+    badge: "Coding",
+    image:
+      "https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1500,
+  },
+  {
+    id: 5,
+    title: "Image Generation with AI",
+    badge: "AI",
+    image:
+      "https://images.pexels.com/photos/3094799/pexels-photo-3094799.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1200,
+  },
+];
+
+const latestTransactions = [
+  {
+    id: 1,
+    title: "Subscription Renewal",
+    badge: "John Doe",
+    image:
+      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1400,
+  },
+  {
+    id: 2,
+    title: "Payment for Services",
+    badge: "Jane Smith",
+    image:
+      "https://images.pexels.com/photos/4969918/pexels-photo-4969918.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 2100,
+  },
+  {
+    id: 3,
+    title: "Subscription Renewal",
+    badge: "Michael Johnson",
+    image:
+      "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1300,
+  },
+  {
+    id: 4,
+    title: "Payment for Services",
+    badge: "Lily Adams",
+    image:
+      "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 2500,
+  },
+  {
+    id: 5,
+    title: "Subscription Renewal",
+    badge: "Sam Brown",
+    image:
+      "https://images.pexels.com/photos/1680175/pexels-photo-1680175.jpeg?auto=compress&cs=tinysrgb&w=800",
+    count: 1400,
+  },
+];
+
+const CardList = ({ title }: { title: string }) => {
+  const list =
+    title === "Popular Content" ? popularContent : latestTransactions;
+  return (
+    <div className="">
+      <h1 className="text-lg font-medium mb-6">{title}</h1>
+      <div className="flex flex-col gap-2">
+        {list.map((item) => (
+          <Card
+            key={item.id}
+            className="flex-row items-center justify-between gap-4 p-4"
+          >
+            <div className="w-12 h-12 rounded-sm relative overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.title}
+                fill
+                className="object-cover"
+              />
+            </div>
+            <CardContent className="flex-1 p-0">
+              <CardTitle className="text-sm font-medium">
+                {item.title}
+              </CardTitle>
+              <Badge variant="secondary">{item.badge}</Badge>
+            </CardContent>
+            <CardFooter className="p-0">{item.count / 1000}K</CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
