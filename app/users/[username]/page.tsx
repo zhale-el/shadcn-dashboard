6ac75e2bfd1ea756582217f335cc7affd26a9c33
zhale-el <zhalehelmi@gmail.com>
2025-09-16 15:22:28 +0330

    sheet

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,3 +1,5 @@
+import CardList from "@/components/CardList";
+import { Badge } from "@/components/ui/badge";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,6 +8,16 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import EditUser from "@/components/EditUser";
 
 const SingleUserPage = () => {
   return (
@@ -25,6 +37,132 @@ const SingleUserPage = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      {/* Container */}
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        {/* Left */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* User badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold p-4 rounded-lg">
+              User Badges
+            </h1>
+
+            <div className="flex gap-4 mt-4">
+              <HoverCard>
+                <HoverCardTrigger>
+                  <BadgeCheck
+                    size={36}
+                    className="rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This user has been verified by the admin.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Shield
+                    size={36}
+                    className="rounded-full bg-green-500/30 border-1 border-green-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Admin</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Admin users have access to all features and can manage
+                    users.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Candy
+                    size={36}
+                    className="rounded-full bg-yellow-500/30 border-1 border-yellow-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Awarded</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This user has been awarded for their contributions.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Citrus
+                    size={36}
+                    className="rounded-full bg-orange-500/30 border-1 border-orange-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Popular</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This user has been popular in the community.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+          </div>
+          {/* Information container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl font-semibold">User Information</h1>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button>Edit User</Button>
+                </SheetTrigger>
+                <EditUser />
+              </Sheet>
+            </div>
+            <div className="space-y-4 mt-4">
+              <div className="flex flex-col gap-2 mb-8">
+                <p className="text-sm text-muted-foreground">
+                  Profile completion
+                </p>
+                <Progress value={66} />
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Username:</span>
+                <span>Maryam.el</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Email:</span>
+                <span>Maryam@gmail.com</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Phone:</span>
+                <span>+98 534567</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Location:</span>
+                <span>Iran, Shz</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Role:</span>
+                <Badge>Admin</Badge>
+              </div>
+            </div>
+            <p className="text-sm text-muted-foreground mt-4">Joined on 2025</p>
+          </div>
+          {/* Card list container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transctions" />
+          </div>
+        </div>
+        {/* Right */}
+        <div className="w-full xl:w-2/3 space-y-6">
+          {/* User card container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
+          {/* Chart container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+        </div>
+      </div>
     </div>
   );
 };
